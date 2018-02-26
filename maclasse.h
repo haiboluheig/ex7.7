28181321be77d977a648d238d32172cec8d3288a
Lu <33836684+haiboluheig@users.noreply.github.com>
2018-02-26 14:59:25 +0100

    Update maclasse.h

--- maclasse.h
+++ maclasse.h
@@ -8,13 +8,13 @@ public:
     C(int x = 0);
     //surchages d'operateurs
     C operator+(const C&) const;
-    C operator++(); //prefix,identique :friend C operator ++(C&)
-    C operator++(const int); //identique :friend C operator ++(C&,int)
+    C& operator++(); //pré-incrémentation,identique :friend C operator ++(C&)
+    C operator++(const int); //post-incrémentationidentique :friend C operator ++(C&,int)
     C operator=(const C&);
     //friend fonctions et surchages d'operateurs
     friend bool operator==(const C&, const C&);
     friend bool operator!=(const C&,const C&);
-    friend C operator+=(C&, const C&);
+    C& operator+=( const C&);
     friend ostream& operator<<(ostream&, const C&);
 private:
     int x;
